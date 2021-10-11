5e4c6c7da0ff481ccc6dc637e36f6f449e2e54e6
jurijsml <90174881+jurijsml@users.noreply.github.com>
2021-10-11 07:12:15 +0000

    erafafaf

---
+++ dada.py
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def pasuti_tkreklus(skaits, apdruka, piegade):
+  cenas = {"TEKSTS":5, "ZIME":7, "FOTO":20}
+
+  apdruka_vert = cenas[apdruka] * skaits
+
+  while piegade:
+    if apdruka_vert < 50:
+      return apdruka_vert + 15
+
+    elif apdruka_vert > 100
+      return apdruka_vert * 0.05
+
+    else:
+      return apdruka_vert
+
+  else:
+    if apdruka_vert <=100:
+      return apdruka_vert
+    else:
+      return apdruka_vert * 0.5
+
+
+
+print(pasuti_tkreklus(6,"TEKSTS",False))
--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-def funkcums ():
-  print('Pirmā funkcija')
-funkcums()
-def funkcums (parcums):
-  print(funkcums, parcums)
-  funkcums(arguments)
-def funkcums (funkcums, gramatas):
-print(funkcums, parcums, gramatas)
+recepte1 = {"cukurs":0.6, "kanelis":0.008, "aboli":2., "udens":0.2}
+cenas1 = {"cukurs":0.75, "kanelis":30.0, "aboli":0., "udens":0.0}
 
+def izmaksas_receptei(recepte,cenas):
+  #Aprēķina izmaksas dotajai receptei
+  summa = 0
+  for sastavdala in recepte:
+    daudzums = recepte[sastavdala]
+    summa += daudzums * cenas[sastavdala]
+
+  return summa 
+
+
+def izmaksas_kopa(abolu_svars, recepte, cenas):
+
+  #izmaksas uz vienu kg ābolu
+  izmaksas_kg = izmaksas receptei(recepte, cenas)
+  /recepte ['aboli']
+
+  ievarijuma_izmaksas = abolu_svars *
+  izmaksas_kg
+  return ievarijuma_izmaksas
+
+print(izmaksas_kopa(15.0, recepte1, cenas1))
